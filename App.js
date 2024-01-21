dc16f7542184eb2e3b785f8c7d847cfe7019c09c
jibontech <jibon.belaface@gmail.com>
2024-01-21 13:29:31 +0600

    remove some code

--- App.js
+++ App.js
@@ -27,21 +27,3 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-
-// import {StyleSheet, Text, View} from 'react-native';
-// import React from 'react';
-// import Icon from 'react-native-vector-icons/dist/FontAwesome';
-
-// const App = () => {
-//   return (
-//     <View>
-//       <Text>
-//         App <Icon name="rocket" size={30} color="#900" />;
-//       </Text>
-//     </View>
-//   );
-// };
-
-// export default App;
-
-// const styles = StyleSheet.create({});
